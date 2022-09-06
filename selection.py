7df70c4d9c05f2a384a1694658d17f1b9450f389
Attila Csikos <nemtudom@hotmail.com>
2022-09-06 16:32:26 +0200

    változtattam

---
+++ selection.py
@@ -0,0 +1,18 @@
+if True:
+    print("Ez mindig lefut")
+
+if False:
+    print("Ez sosem fut le")
+
+if 5 > 100:
+    print("vajon lefut-e")
+
+n = 10
+n % 2 == 0 #Páros
+n % 2 == 1 #Páratlan
+
+#  Kérj be a felhasználótól egy számot! Ha az páros, írd ki, hogy páros.
+#  Ha az páratlan, írd ki, hogy páratlan.
+
+number= int(input ("Írj be egy számot"))
+if True
